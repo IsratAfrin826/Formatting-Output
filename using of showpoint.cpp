11e84c5b06e86d169f13d89d6823e167ac06416c
Israt Arefin <nisratafrin826@gmail.com>
2025-04-29 10:19:15 +0600

    Add files via upload

---
+++ using of showpoint.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+ float num1,num2;
+ cout << "Enter two numbers = ";
+ cin>>num1>>num2 ;
+
+ cout<<showpoint;
+ float sum= num1+num2;
+ cout << "Sum is = " <<sum ;
+ cout<<endl;
+
+
+  cout<<noshowpoint;
+ float sub= num1-num2;
+ cout << "Subtraction is = " <<sub ;
+ cout<<endl;
+
+ cout<<showpoint;
+ float mul= num1*num2;
+ cout << "Multiplicatin is = " <<mul ;
+ cout<<endl;
+
+ cout<<noshowpoint;
+ float div= num1/num2;
+ cout << "Division is = " <<div ;
+ cout<<endl;
+
+ getch();
+}
